21769607a5f48cb9e7f0ff247dbe7e5ef7be9d81
Muzola Ethberg <ethbergmuzola500@gmail.com>
2025-03-31 09:07:24 +0100

    Refactor:Add package pour le translate en francais du calendrier

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:luka_ndaku/screens/LoginPage.dart';
 import 'package:luka_ndaku/screens/Splashscreen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 
 void main() {
@@ -13,6 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('fr', 'FR'), // Français
+      ],
+      locale: const Locale('fr', 'FR'), // Forcer le français
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
