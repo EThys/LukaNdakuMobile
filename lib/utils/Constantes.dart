392bc206a81a45007c7f9e3a79335baf241b1e94
EThys <ethbergmuzola500@gmail.com>
2025-06-07 11:27:02 +0100

    Feat:util pour les constantes notament l'adresse Ip du serveur

---
+++ lib/utils/Constantes.dart
@@ -0,0 +1,6 @@
+class Constantes {
+  static const String BASE_URL = "http://192.168.50.199:8000/api/";
+  static const String IMAGE_URL = "http://192.168.50.199:8000/";
+  static const String DefaultToken="7|HPK4TpJQZN4ADqrE4SrM9IcQyCxsXB0dlH2W5MXR";
+  static const String kIconPath = 'assets/icons';
+}
