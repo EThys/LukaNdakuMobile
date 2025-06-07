649ae1908b4c8bf2766a561c4574ca8ee752d77e
EThys <ethbergmuzola500@gmail.com>
2025-06-07 10:44:49 +0100

    Feat:Mise en place de mes cles pour le local storage

---
+++ lib/utils/StockageKeys.dart
@@ -0,0 +1,13 @@
+
+class StockageKeys{
+  static const String tokenKey="TOKEN";
+  static const String userKey="USER";
+  static const String userPreferenceKey="USERPREFERENCE";
+  static const String searchResultsKey="searchResults";
+  static const String allUsers="ALLUSERS";
+  static const String propreties="propreties";
+  static const String carts="CARTS";
+  static const String orders="ORDERS";
+  static const String listeCampagnes="listeCampagnes";
+  static const String firstLaunchKey="FIRSTLAUNCH";
+}
